60633d7d698f500f6361ceeb0335155ac6a8cd13
kojhyy <l4p0ubelle@gmail.com>
2022-04-30 19:00:38 +0200

    compteur d'étape

--- mylibrary.py
+++ mylibrary.py
@@ -4,15 +4,15 @@
 """
 
 # --- progress bar ---
-
 from time import time as now
 import curses
+import time
 
 class bar:
 
     """progress bar"""
 
-    def __init__(self, steps: int, text: str = "", pattern_bar: str = "█", pattern_space: str = " ", lenght: int = 10, show_time: bool = True) -> None:
+    def __init__(self, steps: int, text: str = "", pattern_bar: str = "█", pattern_space: str = " ", lenght: int = 10, show_steps: bool = True, show_time: bool = True) -> None:
         """initialize the progress bar
 
         Args:
@@ -22,18 +22,21 @@ class bar:
             pattern_space (str): pattern of the space
             lenght (int): lenght of the progress bar
         """
-
         self.steps = steps
         self.text = text
-        self.pattern_bar = pattern_bar
-        self.pattern_space = pattern_space
-        self.lenght = lenght
+        
+        
         self.current = 0
         self.time = now()
         self.init = now()
         self.total = 0
         self.mean = 0
+
         self.show_time = show_time
+        self.show_steps = show_steps
+        self.pattern_bar = pattern_bar
+        self.pattern_space = pattern_space
+        self.lenght = lenght
 
         self.console = curses.initscr()
 
@@ -45,11 +48,10 @@ class bar:
         self.mean += now() - self.time
         self.time = now()
         self.total = self.mean / self.current * self.steps
-        
-        self.console.clear()
-        self.console.addstr(0, 0, f"{self.text} |{self.pattern_bar * (self.current * self.lenght // self.steps)}{self.pattern_space * (self.lenght - (self.current * self.lenght // self.steps))}| {self.current * 100 // self.steps}% {f'[{self.__str_round(self.time - self.init)} s / {self.__str_round(self.total)} s]' if self.show_time else ''}")
+
+        self.console.addstr(0, 0, f"{self.text} | {self.pattern_bar * (self.current * self.lenght // self.steps)}{self.pattern_space * (self.lenght - (self.current * self.lenght // self.steps))}| {self.current * 100 // self.steps}%{' [' if self.show_time or self.show_steps else ''}{f' steps:  {self.current} / {self.steps} ' if self.show_steps else ''}{ '|' if self.show_time and self.show_steps else ''}{f' time: {self.__str_round(self.time - self.init)} s / {self.__str_round(self.total)} s ' if self.show_time else ''}{']' if self.show_time or self.show_steps else ''}")
         self.console.refresh()
-        
+
         if self.current == self.steps:
             curses.endwin()
         
@@ -65,10 +67,6 @@ class bar:
         apres += "0" * (3 - len(apres))
 
         return avant + "." + apres
-    
-    def stop():
-        """Stop the bar"""
-        curses.endwin()
 
 
 
@@ -100,7 +98,6 @@ class string:
         print(a) -> "Hi Earth"\n
         
 
-
         Args:
             string (str): the string to modify
             *args (str): pair of characters to replace
@@ -111,5 +108,3 @@ class string:
         for i in range(0, len(args), 2):
             string = string.replace(args[i], args[i + 1])
         return string
-
-# --- 
